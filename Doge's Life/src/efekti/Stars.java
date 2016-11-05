f7f2c53a381d4b3b709f03a378e265cb935b1ddf
Natasa2604 <natasa2604@yahoo.com>
2016-11-05 13:58:31 +0100

    dodat atribut tipa vector2d

--- Doge's Life/src/efekti/Stars.java
+++ Doge's Life/src/efekti/Stars.java
@@ -2,10 +2,13 @@ package efekti;
 
 import java.awt.Color;
 
+import particleUtil.Vector2d;
+
 public class Stars {
 
 	private String star;
 	private Color color;
+	private Vector2d loc;
 	
 	
 	public Stars() {
